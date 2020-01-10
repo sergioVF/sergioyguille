dbedde8e4017b0cce1df19e67847cd0081b22451
Guillecdl <guille.claret96@gmail.com>
2020-01-10 13:43:20 +0000

    Update db_manager.php

--- userscrud-master/util/db_manager.php
+++ userscrud-master/util/db_manager.php
@@ -67,7 +67,9 @@
     }
 
     function delete_user ($user_id){
-        // COMPLETAR ELIMINAR DE USUARIO
+        $sql = "DELETE FROM user WHERE id='".$user_id."'";
+        $conn = $GLOBALS['conn'];
+        return $conn->query($sql) ;
 
     }
 
